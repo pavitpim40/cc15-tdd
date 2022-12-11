58a5237d823f393eca03e1df64917ae401dc7da3
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-11 13:27:28 +0700

    unit test - post todo

---
+++ controllers/todo.controller.js
@@ -0,0 +1,10 @@
+const TodoModel = require("../model/todo.model");
+
+exports.createTodo = async (req, res, next) => {
+  try {
+    const createdModel = await TodoModel.create(req.body);
+    res.status(201).json(createdModel);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ tests/units/todo.controller.test.js
@@ -0,0 +1,55 @@
+const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
+
+TodoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = jest.fn();
+});
+
+describe("TodoController.getTodos", () => {
+  //getTodos
+  it("should have a getTodos function", () => {
+    expect(typeof TodoController.getTodos).toBe("function");
+  });
+});
+describe("TodoController.createTodo", () => {
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+  it("should have a create Todo function", () => {
+    expect(typeof TodoController.createTodo).toBe("function");
+  });
+
+  it("should call TodoModel.create", () => {
+    TodoController.createTodo(req, res, next);
+    // expect(TodoModel.create).toBeCalled();
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+
+  it("should return 201 response code", async () => {
+    await TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", async () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    await TodoController.createTodo(req, res, next);
+    // expect(res._getJSONData()).toBe(newTodo); // fail because different ref
+    expect(res._getJSONData()).toStrictEqual(newTodo);
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Done property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    TodoModel.create.mockReturnValue(rejectedPromise);
+    await TodoController.createTodo(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  });
+});
