8b5ea775d50d3a7338bffe15275327ee0e80b5cf
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-11 20:17:40 +0700

    integration-test:getTodoById

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -4,5 +4,6 @@ const todoController = require("../controllers/todo.controller");
 
 router.post("/", todoController.createTodo);
 router.get("/", todoController.getTodos);
+router.get("/:todoId", todoController.getTodoById);
 
 module.exports = router;
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -2,7 +2,8 @@ const request = require("supertest");
 const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
 
-const endpointUrl = "/todos";
+const endpointUrl = "/todos/";
+let firstTodo;
 
 describe(endpointUrl, () => {
   test("GET" + endpointUrl, async () => {
@@ -11,6 +12,18 @@ describe(endpointUrl, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
+  });
+  test("Get By Id" + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(endpointUrl + firstTodo._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+
+  test("Get todo by id doesn't exist" + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(endpointUrl + "63945fe2298945af7a03e6a8");
+    expect(response.statusCode).toBe(404);
   });
   it("POST " + endpointUrl, async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
